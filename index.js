e475bc15331ba8cf076597b8da153eccda40813c
Mari Taksa <welch.mari@gmail.com>
2021-06-29 11:49:30 -0500

    commit tree c879b155ac459c8aed47c073ee642c5da2f1fd35

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun (activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (task="go to the office") {
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(flair="*"){
+    return (adj="Special") => `You are ${flair + adj + flair}!`
+}
